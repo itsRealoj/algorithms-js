50f737447448392b1a443d8b0389286a135f66da
itsrealoj <stephenojwang1040@gmail.com>
2020-04-05 21:35:15 +0300

    completed palindrome algorithm

--- isPalindrome.js
+++ isPalindrome.js
@@ -3,9 +3,16 @@ string = string.toLowerCase();
 let stringArr = string.split('');
 let validChars = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+//filter the input string
 let newStringArr = [];
 stringArr.forEach(char => {
  if(validChars.indexOf(char) > -1) newStringArr.push(char);
   else console.log(newStringArr);
  });
+
+//doing the compaprison
+if (newStringArr.join('') === newStringArr.reverse().join('')) console.log('You got a Palindrome !');
+else console.log('nothing near a Palindrome!');
 }
+         
+isPalindrome('r;ace car');
